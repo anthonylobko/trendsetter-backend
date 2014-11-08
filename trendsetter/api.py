65210030ea107ffc0da27059ce9add1b5bc67565
James <mish0116@umn.edu>
2014-11-08 09:48:52 -0800

    Basic POST and SQLAlchemy code

--- trendsetter/api.py
+++ trendsetter/api.py
@@ -1,8 +1,35 @@
 import os
-from flask import Flask
+from flask import Flask, request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+
+def post_ideas(username, idea, device_id):
+    """
+    Write a single idea to the database.
+
+    Arguments:
+     * username: The username of the person that posted the idea.
+     * idea: A string representing the entire idea
+     * device_id: The ID for the device the idea was submitted
+       from.
+    """
 
 @app.route("/")
 def hello():
     return "Hello world!"
+
+
+@app.route("/post", methods=["POST", "GET"])
+def post():
+    if request.method == "POST":
+        return post_ideas(
+            request.form["username"]
+            request.form{"idea"]
+            request.form["device_id"]
+            )
+    else:
+        return "No post stuff?"
