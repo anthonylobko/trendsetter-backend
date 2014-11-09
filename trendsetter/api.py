101704d171d86f90e334dce92c122f0722cfa82f
James <mish0116@umn.edu>
2014-11-08 18:08:27 -0800

    Putting everything in 1 file..

--- trendsetter/api.py
+++ trendsetter/api.py
@@ -1,7 +1,19 @@
 import os
-from flask import request
-from trendsetter import models
-from setup import app, db
+from flask import Flask, request
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, \
+    Integer,\
+    String, \
+    Text, \
+    ForeignKey
+from sqlalchemy.orm import relationship
+
+
+app = Flask(__name__)
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 def post_ideas(username, idea):
     """
@@ -34,3 +46,38 @@ def post():
             )
     else:
         return "No post stuff?"
+
+class Idea(db.Model):
+    """
+    A representation of a single idea.
+    """
+    __tablename__ = 'ideas'
+
+    id = Column(Integer,
+                primary_key=True,
+                nullable=False)
+
+    username = Column(String(256),
+                      ForeignKey("User.username"),
+                      nullable=False)
+
+    idea = Column(Text,
+                  nullable=False)
+
+
+class User(db.Model):
+    """
+    A single user in the system
+    """
+    __tablename__ = 'users'
+
+
+    id = Column(Integer,
+                primary_key=True,
+                nullable=False)
+
+    username = Column(String(256),
+                      unique=True,
+                      nullable=False)
+
+    ideas = relationship("Idea")
