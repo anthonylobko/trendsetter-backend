68f56e819e8714fc921d35d416e3e31ed8305755
James <mish0116@umn.edu>
2014-11-08 19:26:54 -0800

    Added getting information

--- trendsetter/api.py
+++ trendsetter/api.py
@@ -9,12 +9,14 @@ from sqlalchemy import Column, \
     Text, \
     ForeignKey
 from sqlalchemy.orm import relationship
+import json
 
 
 app = Flask(__name__)
 app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+
 
 def post_ideas(username, idea):
     """
@@ -37,6 +39,19 @@ def hello():
     return "Hello world!"
 
 
+@app.route("/get/<int:limit>/<int:offset>")
+def get(limit, offset):
+    return json.dumps(
+        [
+            {"username": item.username,
+             "idea": item.idea}
+            for item
+            in session.query(Idea).order_by(Idea.id)
+            [offset:offset + limit]
+        ]
+    )
+
+
 @app.route("/post", methods=["POST", "GET"])
 def post():
     if request.method == "POST":
@@ -46,6 +61,7 @@ def post():
             )
     else:
         return "No post stuff?"
+
 
 class Idea(db.Model):
     """
@@ -58,30 +74,7 @@ class Idea(db.Model):
                 nullable=False)
 
     username = Column(String(256),
-#                      ForeignKey("User.username"),
                       nullable=False)
 
     idea = Column(Text,
                   nullable=False)
-
-
-Base = declarative_base()
-Engine = create_engine( os.environ['DATABASE_URL'] )
-Base.metadata.create_all(Engine)
-
-# class User(db.Model):
-#     """
-#     A single user in the system
-#     """
-#     __tablename__ = 'users'
-
-
-#     id = Column(Integer,
-#                 primary_key=True,
-#                 nullable=False)
-
-#     username = Column(String(256),
-#                       unique=True,
-#                       nullable=False)
-
-#     ideas = relationship("Idea.username")
